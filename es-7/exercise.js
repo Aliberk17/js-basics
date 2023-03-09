b9ad59ff4430020d2c89c8005939abcef70aa765
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-09 19:57:14 +0300

    ex-7

--- es-7/exercise.js
+++ es-7/exercise.js
@@ -1,3 +1,10 @@
-let hello = 'Ciao';     // number
-let age = 18;       // boolean
-let isGraduated = false;     // string
+let hello = Number('Ciao');     // number
+let age = Boolean(18);       // boolean
+let isGraduated = false.toString();     // string
+
+console.log(hello);
+console.log(age);
+console.log(isGraduated);
+
+// Hello output is NaN (not a number) because it is not convertable to
+// a numeric type.

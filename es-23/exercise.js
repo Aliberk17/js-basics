85dbd22e86a07906220c2e97a230633f702c9d70
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-09 21:49:52 +0300

    ex-23

---
+++ es-23/exercise.js
@@ -0,0 +1,12 @@
+let price = 80;
+switch (price) {
+  case 50:
+    console.log("Basic Room");
+    break;
+  case 80:
+    console.log("Junior Master Room");
+    break;
+  case 100:
+    console.log("Master Room");
+    break;
+}

3a019bde1b6ea21cff38a664880522c86b492299
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-09 21:45:45 +0300

    ex-22

---
+++ es-22/exercise.js
@@ -0,0 +1,13 @@
+let primitive = 12;
+
+switch(typeof(primitive)){
+    case "number": 
+      console.log("primitive is a number.");
+      break;
+      case "string":
+        console.log("primitive is a string.");
+       break;
+       case "boolean":
+       console.log("primitive is a boolean.")
+       break;
+}

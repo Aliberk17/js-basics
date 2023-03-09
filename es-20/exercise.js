7f041521264b8b2eadfb0d96dd20bc6183f8cd6a
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-09 21:32:57 +0300

    ex-20

---
+++ es-20/exercise.js
@@ -0,0 +1,9 @@
+let primitive = 12;
+
+if (typeof(primitive) === "number"){
+    console.log("primitive is a number.");
+}  else if(typeof(primitive) == "string"){
+    console.log("primitive is a string.");
+}  else {
+    console.log("primitive is a boolean.");
+}

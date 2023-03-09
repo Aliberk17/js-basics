d55023ee98bbfa9b348b809d09a095a1774bfb2f
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-09 21:36:03 +0300

    ex-21

--- es-21/exercise.js
+++ es-21/exercise.js
@@ -1,1 +1,6 @@
-const password = 'Hi';
+const password = 'cetin';
+if (4<password.length && password.length<6){
+    console.log("Password length is correct");
+}  else{
+    console.log("Password not valid");
+}

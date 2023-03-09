bf5194c42d23c63adfb2015886b40807d0b9bae3
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-09 20:21:34 +0300

    ex-10

--- es-10/exercise.js
+++ es-10/exercise.js
@@ -1,5 +1,8 @@
 const firstYearCompleted = true;
 const yearsCompleted = 1;
 
-yearsCompleted == firstYearCompleted
-yearsCompleted === firstYearCompleted
+yearsCompleted == firstYearCompleted;
+yearsCompleted === firstYearCompleted;
+
+// double equals are for the checking the values. if we use triple equals,
+// both value and type should be exact same.

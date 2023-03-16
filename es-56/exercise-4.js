29c19eec65d3726de5563ad0d365c78892aac425
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-16 21:18:00 +0300

    ex-56

---
+++ es-56/exercise-4.js
@@ -0,0 +1,66 @@
+function uncompletedNotes(notes) {
+  let undone = [];
+  notes.forEach(el => {
+    let fail = el.todos.filter((x)=>{
+      if(x.done != true){
+        return x;
+      }
+    }
+    )
+    undone = undone.concat(fail);
+  });
+  return undone;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+
+console.log(uncompletedNotes(notes));

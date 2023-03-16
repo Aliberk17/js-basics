e039da70f32cbf4491af2d28e194f1d36f85f785
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-03-16 12:16:06 +0300

    ex35

---
+++ es-35/exercise.js
@@ -0,0 +1,13 @@
+function canPlay() {
+  let sport = " Football";
+
+  if (true) {
+    var personName = "Cosimo";
+  }
+
+  console.log(personName + sport);
+}
+
+canPlay();
+//Inside of if block, "let" can't go out of the block. We should use
+// var instead.
